c4fbee70255fc9c4554f1cf4c418a37a1ad93c7e
John Oss <99jao99@gmail.com>
2017-01-13 08:50:27 -0500

    Basic File Layout Creation
    Main class created,
    View class created,
    Locker class created

---
+++ src/lockd/Locker.java
@@ -0,0 +1,15 @@
+package lockd;
+
+/**
+ * @author johnandrewoss
+ * @date 01/11/2017
+ * @title Locker.java
+ * @purpose Container for all of the passwords.
+ * 
+ */
+
+public class Locker {
+    public Locker(){
+    
+    }
+}
